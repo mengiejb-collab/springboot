4e2d89fa9774af29adbdf0783b50d00beae565a6
Mengie Jean-Baptiste <mengiejean-baptiste@Mengies-MacBook-Air.local>
2025-10-25 16:58:06 -0400

    Initial commit: Sprint Boot Project

---
+++ project.java
@@ -0,0 +1,7 @@
+springboot-java-demo/
+ ├─ src/
+ │   ├─ main/java/com/example/demo/DemoApplication.java
+ │   └─ test/java/com/example/demo/DemoApplicationTests.java
+ ├─ pom.xml  (or build.gradle)
+ └─ .gitignore
+
